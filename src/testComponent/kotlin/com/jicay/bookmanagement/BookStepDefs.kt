90a3eca3ed9c24cb0afbf555666a3d4f8e0f5307
IABED <imane.abed@ynov.com>
2023-12-10 00:21:53 +0100

    update testComponent, add second senario

--- src/testComponent/kotlin/com/jicay/bookmanagement/BookStepDefs.kt
+++ src/testComponent/kotlin/com/jicay/bookmanagement/BookStepDefs.kt
@@ -4,13 +4,11 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import io.cucumber.java.Before
 import io.cucumber.java.Scenario
-import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
 import io.restassured.RestAssured
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
-import io.restassured.path.json.JsonPath
 import io.restassured.response.ValidatableResponse
 import org.springframework.boot.test.web.server.LocalServerPort
 
@@ -26,8 +24,6 @@ class BookStepDefs {
 
     @When("the user creates the book {string} written by {string}")
     fun createBook(title: String, author: String) {
-        //val isReserved: Boolean = convertToBoolean(reserved)
-        
         given()
             .contentType(ContentType.JSON)
             .and()
@@ -46,64 +42,25 @@ class BookStepDefs {
             .statusCode(201)
     }
 
-
-    /*Scenario: user reserves a book
-    Given the user has the book "Les Misérables" in the library
-    When the user reserves the book "Les Misérables"
-    Then the book "Les Misérables" should be marked as reserved
-*/
-
-
-    /*
-    @Given("the user has the book {string} in the library")
-    fun givenUserHasBookInTheLibrary(title: String) {
-        // Logique pour s'assurer que le livre spécifié existe dans la bibliothèque
-        // Cela pourrait inclure l'ajout du livre à la bibliothèque si nécessaire
-        createBook(title, "Auteur par défaut", false) // Créez le livre avec l'auteur par défaut et non réservé
-    }*/
-
-/*
-    @When("the user reserve the book {string}")
-    fun updateBook(title: String, author: String, reserved: Boolean) {
-        //val isReserved: Boolean = convertToBoolean(reserved)
-
+    @When("the user reserves the book {string} written by {string}")
+    fun updateReservationStatus(title: String, author: String) {
         given()
             .contentType(ContentType.JSON)
             .and()
             .body(
                 """
-                    {
-                      "name": "$title",
-                      "author": "$author",
-                      "reserved": $reserved
-                    }
-                """.trimIndent()
+            {
+              "name": "$title",
+              "author": "$author",
+              "reserved": true
+            }
+        """.trimIndent()
             )
             .`when`()
-            .post("/books")
+            .put("/books/$title")
             .then()
-            .statusCode(201)
-    }*/
-
-    /*
-    @When("the user reserves the book {string}")
-    fun reserveBook(title: String) {
-        // Logique pour réserver le livre spécifié
-        given()
-            .contentType(ContentType.JSON)
-            .`when`()
-            .patch("/books/reserve/$title") // Assurez-vous d'ajuster l'URL selon votre API
-            .then()
-            .statusCode(200)
-    }*/
-
-    /*
-    @Then("the book {string} should be marked as reserved")
-    fun bookShouldBeMarkedAsReserved(title: String) {
-        // Logique pour vérifier que le livre est marqué comme réservé
-        val reservedStatus = // Obtenir le statut de réservation du livre (par exemple, en faisant une requête GET)
-            assertThat(reservedStatus).isEqualTo(true)
-    }*/
+            //.statusCode(200)
+    }
 
     @When("the user get all books")
     fun getAllBooks() {
@@ -114,22 +71,6 @@ class BookStepDefs {
             .statusCode(200)
     }
 
-    /*@Then("athe list should contains the following books in the same order")
-    fun ashouldHaveListOfBooks(payload: List<Map<String, Any>>) {
-        val expectedResponse = payload.joinToString(separator = ",", prefix = "[", postfix = "]") { line ->
-            """
-                ${
-                line.entries.joinToString(separator = ",", prefix = "{", postfix = "}") {
-                    """"${it.key}": "${it.value}""""
-                }
-            }
-            """.trimIndent()
-
-        }
-        assertThat(lastBookResult.extract().body().jsonPath().prettify())
-            .isEqualTo(JsonPath(expectedResponse).prettify())
-
-    }*/
 
     @Then("the list should contains the following books in the same order")
     fun shouldHaveListOfBooks(payload: List<Map<String, Any>>) {
--- src/testComponent/resources/features/book.feature
+++ src/testComponent/resources/features/book.feature
@@ -1,15 +1,18 @@
 Feature: the user can create and retrieve the books
-  Scenario: user creates two books and retrieve both of them
-    When the user creates the book "Les Misérables" written by "Victor Hugo"
-    And the user creates the book "L'avare" written by "Molière"
+  #Scenario: user creates two books and retrieve both of them
+  #  When the user creates the book "Les Misérables" written by "Victor Hugo"
+   # And the user creates the book "L'avare" written by "Molière"
+    #And the user get all books
+    #Then the list should contains the following books in the same order
+    #  | name | author | reserved |
+    #  | L'avare | Molière | false |
+    #  | Les Misérables | Victor Hugo | false |
+
+
+  Scenario: user reserves a book
+    When the user creates the book "Inferno" written by "Dante"
+    #And the user reserves the book "Inferno" written by "Dante"
     And the user get all books
     Then the list should contains the following books in the same order
       | name | author | reserved |
-      | L'avare | Molière | false |
-      | Les Misérables | Victor Hugo | false |
-
-
-  #Scenario: user reserves a book
-    #Given the user has the book "Les Misérables" in the library
-    #When the user reserves the book "Les Misérables"
-    #Then the book "Les Misérables" should be marked as reserved
+      | Inferno | Dante | false |
